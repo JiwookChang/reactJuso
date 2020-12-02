a171b892651db0e3d3092fb66f2c8d7621152ef8
JiwookChang <jiwookc80@gmail.com>
2020-12-02 11:54:24 +0900

    road view work

--- src/pages/JusoPage.tsx
+++ src/pages/JusoPage.tsx
@@ -17,11 +17,25 @@ declare global {
     kakao: any;
   }
 }
-
+// map elements
 let container = null;
 let options = null;
 let map = null;
 let marker = null;
+let facilityMarkers = [];
+
+// roadView elements
+let rvContainer = null;
+let rv = null;
+let rc = null;
+let rvResetValue = {"panoId":null, "pan":null, "tilt":null,"zoom":null};
+
+
+const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',   // 이미지 주소 
+      imageSize = new window.kakao.maps.Size(35, 38),                                         // 마커이미지의 크기입니다
+      imageOption = {offset: new  window.kakao.maps.Point(27, 69)};                           // 마커이미지의 옵션
+
+const facilityMarkerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
 function onClick(query, ldongCd, stNmCd, bldMainNum, bldSubNum, jihaChk) {
   axios({
@@ -102,7 +116,9 @@ const mapSetting = (mode, x, y) => {
     });
     marker.setMap(map);
     map.setCenter(new window.kakao.maps.LatLng(y, x));
-
+    options.center = new window.kakao.maps.LatLng(y, x);
+    
+    roadViewSetting("change");
     getFacilityInfoByXY(y, x);
   }
 };
@@ -119,29 +135,75 @@ const getFacilityInfo = (ldongCd, stNmCd, bldMainNum, bldSubNum, jihaChk) => {
     })  
 };
 
-const getFacilityInfoByXY = (x, y) => {  
-  console.log("posX = "+x+"   posY = "+y);
-  
+const getFacilityInfoByXY = (x, y) => {
+  clearMarkers();  
+
   const params = "/findPosContent?posX="+x+"&posY="+y;
-  
   axios.get('http://a2fb35f700d7c4890a4b9643dfc0a82b-464956859.ap-northeast-2.elb.amazonaws.com:8080/juso/content'+params)
     .then(res => {
       let allRepos = Array.from(res.data);  // axios를 통해 온 객체는 HTㅢCollection이다. Javascript Array로 변경해 map을 사용할 수 있다.
       console.log(allRepos);
-
       
       for(var i=0;i<allRepos.length; i++){
         const mark = new window.kakao.maps.Marker({
           position: new window.kakao.maps.LatLng(allRepos[i]['posX'], allRepos[i]['posY']),
-          map: map,
+          image: facilityMarkerImage
         });
-
-        mark.setMap(map);
+        facilityMarkers.push(mark);
       }
+      setMarkers();
     })  
 };
 
+const clearMarkers = () => { 
+  if(facilityMarkers.length>0){
+    for(var i=0;i<facilityMarkers.length; i++){
+      facilityMarkers[i].setMap(null);        // Clear markers on map
+    }
+    facilityMarkers = [];                     // Clear markers
+  }
+}
 
+const setMarkers = () => { 
+  for (var i = 0; i < facilityMarkers.length; i++) {
+    facilityMarkers[i].setMap(map);
+  }  
+}
+
+const roadViewSetting = (mode) => {  
+  if(mode === "start"){
+    rvContainer = document.getElementById('roadview'); //지도를 담을 영역의 DOM 레퍼런스
+    rv = new window.kakao.maps.Roadview(rvContainer); // 로드뷰 객체 생성
+    rc = new window.kakao.maps.RoadviewClient(); // 좌표를 통한 로드뷰의 panoid를 추출하기 위한 로드뷰 help객체 생성
+    
+    rc.getNearestPanoId(options.center, 50, function(panoId) {
+        rv.setPanoId(panoId, options.center);//좌표에 근접한 panoId를 통해 로드뷰를 실행합니다.
+        rvResetValue.panoId = panoId;
+    });
+  }else{
+    console.log("options.center > "+options.center);
+    rc.getNearestPanoId(options.center, 50, function(panoId) {
+      rv.setPanoId(panoId, options.center);//좌표에 근접한 panoId를 통해 로드뷰를 실행합니다.
+      rvResetValue.panoId = panoId;
+    });
+  }
+
+};
+/*
+const rvInit = () => {  
+  // 로드뷰 마커가 중앙에 오도록 로드뷰의 viewpoint 조정 합니다.
+  const projection = rv.getProjection(); // viewpoint(화면좌표)값을 추출할 수 있는 projection 객체를 가져옵니다.
+  
+  // 마커의 position과 altitude값을 통해 viewpoint값(화면좌표)를 추출합니다.
+  const viewpoint = projection.viewpointFromCoords(marker.getPosition(), marker.getAltitude());
+  rv.setViewpoint(viewpoint); //로드뷰에 뷰포인트를 설정합니다.
+
+  //각 뷰포인트 값을 초기화를 위해 저장해 놓습니다.
+  rvResetValue.pan = viewpoint.pan;
+  rvResetValue.tilt = viewpoint.tilt;
+  rvResetValue.zoom = viewpoint.zoom;
+};
+*/
 
 const App: React.FC = () => {
   const ListLoading = jusoList;
@@ -174,6 +236,7 @@ const App: React.FC = () => {
 
   useEffect(() => {
     mapSetting("start",'','');
+    roadViewSetting("start");
   }, [])
   
   return (
@@ -209,7 +272,8 @@ const App: React.FC = () => {
             </div>
           </Grid>
           <Grid item xs={12} md={6}>
-            <div id="map" style={{ width: "100%", height: "65vh"}} />
+            <div id="map" style={{ width: "100%", height: "32vh"}} />
+            <div id="roadview" style={{ width: "100%", height: "32vh"}} />
           </Grid>
         </Grid>
       </div>
